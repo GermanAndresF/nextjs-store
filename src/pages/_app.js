958efdc560e4b21760468b2dbd95ce9c0a3a4ee6
GermanAndresF <gfgermanflechas5@gmail.com>
2023-04-18 13:30:10 -0500

    next/script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,24 @@
-import '../styles/globals.css'
 import AppContext from '@context/AppContext';
+import Script from 'next/script';
 import useInitialState from '@hooks/useInitialState';
 import Header from '@components/Header';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XMZ26PJ5JV" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());                      
+        gtag('config', 'G-XMZ26PJ5JV');`}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
   );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,27 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    <title>React Shop</title>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XMZ26PJ5JV"></script>
-                    <script dangerouslySetInnerHTML={{
-                        html: `window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());                      
-                        gtag('config', 'G-XMZ26PJ5JV');`
-                    }}/>
-
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
-};
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 export default MyDocument;

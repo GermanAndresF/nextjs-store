ea7de2f271202b639741da870815f392bda79ea7
GermanAndresF <gfgermanflechas5@gmail.com>
2023-04-25 10:04:48 -0500

    Progressive Web Apps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['picsum.photos', 'futureworld.com.vn', 'api.escuelajs.co', 'placeimg.com', 'api.lorem.space']
-  }
-}
+    domains: ['picsum.photos', 'futureworld.com.vn', 'api.escuelajs.co', 'placeimg.com', 'api.lorem.space'],
+  },
+};
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
